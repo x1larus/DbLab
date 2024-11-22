2f0f153e29d84f6228167f4bae812912ad59b79a
Alexey Khurtin <alexei.hurtin@gmail.com>
2024-11-22 12:25:04 +0300

    wpf. начало

---
+++ DbLab.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DbLab.WpfApp.Extensions;
+using System.Windows;
+
+namespace DbLab.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class MainWindowModel : PropertyChangedModel
+    {
+
+    }
+}
