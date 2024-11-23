4dcff3e27a6ca17e1e7a355dc511939531137385
Alexey Khurtin <alexei.hurtin@gmail.com>
2024-11-23 16:49:07 +0300

    попытка настроить контролы

---
+++ DbLab.WpfApp/Controls/DashboardControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DbLab.WpfApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for DashboardControl.xaml
+    /// </summary>
+    public partial class DashboardControl : UserControl
+    {
+        public DashboardControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DbLab.WpfApp/MainWindow.xaml.cs
+++ DbLab.WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using DbLab.WpfApp.Extensions;
+﻿using DbLab.WpfApp.Controls;
+using DbLab.WpfApp.Extensions;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DbLab.WpfApp
 {
@@ -8,14 +11,46 @@ namespace DbLab.WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainWindowModel _model;
         public MainWindow()
         {
             InitializeComponent();
+            _model = new MainWindowModel();
+            _model.Tabs.Add(new TabModel { TabName = "Главная", Control = typeof(DashboardControl) });
+            _model.Tabs.Add(new TabModel { TabName = "хуй", Control = typeof(DashboardControl) });
+            DataContext = _model;
+        }
+
+        private void Selector_OnSelected(object sender, RoutedEventArgs e)
+        {
+            if (_model.SelectedTab == null) return;
+
+            _model.CurrentControl = Activator.CreateInstance(_model.SelectedTab.Control) as UserControl;
         }
     }
 
     public class MainWindowModel : PropertyChangedModel
     {
+        private UserControl? _currentControl;
+        public ObservableCollection<TabModel> Tabs { get; set; } = new ObservableCollection<TabModel>();
 
+        public TabModel? SelectedTab { get; set; }
+
+        public UserControl? CurrentControl
+        {
+            get => _currentControl;
+            set
+            {
+                _currentControl = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class TabModel : PropertyChangedModel
+    {
+        public string TabName { get; set; }
+
+        public Type Control { get; set; }
     }
 }
