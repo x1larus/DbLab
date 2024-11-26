65007bccc450dbf746beb323d443c942e87556d0
Алексей Хуртин <alexei.hurtin@gmail.com>
2024-11-27 00:51:18 +0300

    AddIncomeWindow

---
+++ DbLab.WpfApp/Windows/AddIncomeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using DbLab.WpfApp.Base;
+
+namespace DbLab.WpfApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddIncomeWindow.xaml
+    /// </summary>
+    public partial class AddIncomeWindow : Window
+    {
+        public AddIncomeWindow(AddIncomeWindowModel model)
+        {
+            model.Parent = this;
+            InitializeComponent();
+        }
+    }
+
+    public class AddIncomeWindowModel : NotifyPropertyChangedItem
+    {
+        public AddIncomeWindow Parent;
+
+        private DateTime? _date;
+        private decimal? _sum;
+        private long? _type;
+        private string _comment;
+    }
+}
